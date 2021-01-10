bf709119ffd9cd209d3ca304fc12dad0a182aaa4
mostafizurrahman533 <mdfahad01618@gmail.com>
2021-01-10 11:21:27 +0600

    update

--- assets/js/function.js
+++ assets/js/function.js
@@ -163,63 +163,65 @@
     }
   });
 
-
-
-
-
-
-  $('a[data-rel=lightcase]').lightcase({
-    // Will be called immediately after lightcase is initialized
-    onInit : {
-      foo: function() {
-        alert('Lightcase process is initialized');
-      }
-    },
-    // Will be called before generating content
-    onStart : {
-      bar: function() {
-        alert('Lightcase process is started');
-      }
-    },
-    // Will be called right before new dimensions get calculated
-    onBeforeCalculateDimensions : {
-      baz: function() {
-        alert('Lightcase process is calculating new dimensions');
-      }
-    },
-    // Will be called right after new dimensions were calculated
-    onAfterCalculateDimensions : {
-      qux: function() {
-        alert('Lightcase process has calculated new dimensions');
-      }
-    },
-    // Will be called right before the content will be displayed
-    onBeforeShow : {
-      quux: function() {
-        alert('Lightcase process will show the content now');
-      }
-    },
-    // Will be called after everything is loaded and visible
-    onFinish : {
-      corge: function() {
-        alert('Lightcase process has finished');
-      }
-    },
-    // Will be called when aborting lightcase
-    onClose : {
-      grault: function() {
-        alert('Lightcase closes now');
-      }
-    },
-    // Will be called when lightcase does the cleanup
-    onCleanup : {
-      garply: function() {
-        alert('Lightcase process is cleaning up');
-      }
-    }
-  });
-
 })(jQuery);
 
 
 
+/* Store the element in el */
+let el = document.getElementById('tilt')
+
+/* Get the height and width of the element */
+const height = el.clientHeight
+const width = el.clientWidth
+
+/*
+  * Add a listener for mousemove event
+  * Which will trigger function 'handleMove'
+  * On mousemove
+  */
+el.addEventListener('mousemove', handleMove)
+
+/* Define function a */
+function handleMove(e) {
+  /*
+    * Get position of mouse cursor
+    * With respect to the element
+    * On mouseover
+    */
+  /* Store the x position */
+  const xVal = e.layerX
+  /* Store the y position */
+  const yVal = e.layerY
+  
+  /*
+    * Calculate rotation valuee along the Y-axis
+    * Here the multiplier 20 is to
+    * Control the rotation
+    * You can change the value and see the results
+    */
+  const yRotation = 10 * ((xVal - width / 2) / width)
+  
+  /* Calculate the rotation along the X-axis */
+  const xRotation = -10 * ((yVal - height / 2) / height)
+  
+  /* Generate string for CSS transform property */
+  const string = 'perspective(500px) scale(1) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)'
+  
+  /* Apply the calculated transformation */
+  el.style.transform = string
+}
+
+/* Add listener for mouseout event, remove the rotation */
+el.addEventListener('mouseout', function() {
+  el.style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)'
+})
+
+/* Add listener for mousedown event, to simulate click */
+el.addEventListener('mousedown', function() {
+  el.style.transform = 'perspective(500px) scale(0.9) rotateX(0) rotateY(0)'
+})
+
+/* Add listener for mouseup, simulate release of mouse click */
+el.addEventListener('mouseup', function() {
+  el.style.transform = 'perspective(500px) scale(1.1) rotateX(0) rotateY(0)'
+})
